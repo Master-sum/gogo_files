fbb3014544f7f521b3c3db21cacf50bf68f6febf
Master-sum <285957109@qq.com>
2020-11-17 22:59:22 +0800

    frist go

---
+++ http.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+func main()  {
+	var str string = "bbb"
+	var a, b int = 1,2
+	x:=1
+	fmt.Print(x,str,a,b)
+}
---
+++ var_use.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var i int
+	const a,b = 1,2
+	fmt.Print("%q\n",i)
+	fmt.Print(b-a)
+}
