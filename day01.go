c367f552b91c3b07af517230e915a139ff6dc9e0
Master-sum <285957109@qq.com>
2020-11-18 23:46:49 +0800

    for

---
+++ day01.go
@@ -0,0 +1,16 @@
+/*
+作者   ：bjx
+创建时间   ：2020/11/18  10:52 下午
+文件名称   ：day01.PY
+开发工具   ：GoLand
+*/
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Print(i)
+	}
+
+}
