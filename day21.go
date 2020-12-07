4d4d3ab4ea14e37939fc7f87d8f54741446e77dd
Master-sum <285957109@qq.com>
2020-12-07 23:56:36 +0800

    闭包函数

---
+++ day21.go
@@ -0,0 +1,26 @@
+/*
+作者   ：bjx
+创建时间   ：2020/12/7  11:50 下午
+文件名称   ：day21.PY
+开发工具   ：GoLand
+*/
+package main
+
+import "fmt"
+
+//创建一个玩家生成器，输入名字，输出生成器
+func play(name string) func() (string, int) {
+	hp := 160
+	//创建一个闭包
+	return func() (string, int) {
+		//返回
+		return name, hp
+	}
+}
+func main() {
+
+	g := play("hello")
+	name, hp := g()
+	fmt.Println(name, hp)
+
+}
